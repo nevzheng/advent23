ab06ef65264ee6cc0ab08d9a2a8cae3a02a846af
Nevin Zheng <nevzheng@gmail.com>
2023-12-01 22:57:58 +0000

    solve 1.2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,8 +1,29 @@
 advent_of_code::solution!(1);
 
-fn line_value(line: &str) -> Option<u32> {
-    dbg!(line);
+#[macro_use]
+extern crate lazy_static;
 
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref WORD_VALUES: HashMap<&'static str, u32> = {
+        let mut m = HashMap::new();
+
+        m.insert("one", 1);
+        m.insert("two", 2);
+        m.insert("three", 3);
+        m.insert("four", 4);
+        m.insert("five", 5);
+        m.insert("six", 6);
+        m.insert("seven", 7);
+        m.insert("eight", 8);
+        m.insert("nine", 9);
+
+        m
+    };
+}
+
+fn line_value_digits(line: &str) -> Option<u32> {
     let digits: Vec<char> = line.chars().filter(char::is_ascii_digit).collect();
     if digits.is_empty() {
         panic!("line did not have any digits: {}", line);
@@ -14,12 +35,39 @@ fn line_value(line: &str) -> Option<u32> {
     Some(val)
 }
 
+fn get_word_value(current_word: &str) -> Option<u32> {
+    if let Ok(digit) = current_word.parse::<u32>() {
+        return Some(digit);
+    }
+
+    WORD_VALUES.get(current_word).copied()
+}
+
+fn line_value_words(line: &str) -> Option<u32> {
+    let mut digits: Vec<u32> = vec![];
+
+    for i in 0..line.len() {
+        for j in i..=line.len() {
+            let slice = &line[i..j];
+
+            if let Some(val) = get_word_value(slice) {
+                digits.push(val);
+            }
+        }
+    }
+
+    let first = digits.first()?;
+    let last = digits.last()?;
+    let val = (10 * first) + last;
+    Some(val)
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    input.lines().map(line_value).sum()
+    input.lines().map(line_value_digits).sum()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    input.lines().map(line_value_words).sum()
 }
 
 #[cfg(test)]
@@ -32,9 +80,17 @@ mod tests {
         assert_eq!(result, Some(142));
     }
 
-    // #[test]
-    // fn test_part_two() {
-    //     let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-    //     assert_eq!(result, None);
-    // }
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(142));
+    }
+
+    #[test]
+    fn test_part_two_example_two() {
+        // Words May Overlap
+        let example = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen";
+        let result = part_two(example);
+        assert_eq!(result, Some(281));
+    }
 }
